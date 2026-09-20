404412f8bc21c5162b4835b83f38ba3bc82b567c
SumitYadav-07 <sumit.p.yadav07@gmail.com>
2026-09-20 22:39:13 +0530

    Remove error from App.java and rename it

--- backend/Authentication.java
+++ backend/Authentication.java
@@ -1,31 +1,35 @@
-import java.sql.SQLOutput;
+package backend;
+
 import java.util.HashMap;
 import java.util.Scanner;
 public class Authentication{
 
     public static void Signin(HashMap<String,String> user_details){
-        Scanner sc =new Scanner(System.in);
+        Scanner input =new Scanner(System.in);
         System.out.print("Enter your username: ");
-        String username=sc.next();
+        String username=input.next();
         System.out.print("Enter your password: ");
-        String password=sc.next();
+        String password=input.next();
         if(user_details.containsKey(username)){
             System.out.println("Username already exist choose different username.");
             Signin(user_details);
+            input.close();
             return;
         }
 
         user_details.put(username,password);
 
         System.out.println("Sign in successful.");
+        input.close();
     }
 
+
     public static void login(HashMap<String,String> user_details){
-        Scanner sc=new Scanner(System.in);
+        Scanner sca=new Scanner(System.in);
         System.out.print("Enter your username: ");
-        String username=sc.next();
+        String username=sca.next();
         System.out.print("Enter your password: ");
-        String password=sc.next();
+        String password=sca.next();
         if(user_details.containsKey(username)){
             if(password.equals(user_details.get(username))){
                 System.out.print("login successful.");
@@ -33,5 +37,8 @@ public class Authentication{
             System.out.print("Invalid password");
         }
         System.out.println("Invalid Username.");
+        sca.close();
     }
+    
+    
 }
--- backend/app.java
+++ backend/app.java
@@ -1,21 +1,23 @@
+package backend;
+
 import java.util.Scanner;
-
-public class App {
-
+import java.util.HashMap;
+public class App{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        HashMap<String,String> user_details=new HashMap<>();
 
         System.out.println("Enter what you want to do (login/signin):");
         String answer = sc.next();
-
         if (answer.equalsIgnoreCase("signin")) {
-            Authentication.Signin();
+            Authentication.Signin(user_details);
         }
         else if (answer.equalsIgnoreCase("login")) {
-            Authentication.login();
+            Authentication.login(user_details);
         }
         else {
             System.out.println("Invalid input.");
         }
+        sc.close();
     }
-}x
+}
